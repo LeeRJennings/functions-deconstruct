3e2dd559fe797322c9bb60d3a0f5df570682bce2
LeeRJennings <leerjennings@outlook.com>
2022-02-17 13:40:49 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,66 @@
+// ================== play Ultimate, but in functions ================== //
+
+const checkLocation = () => {
+    console.log("1. check GroupMe for the location and time of the game")
+}
+
+const getDressed = () => {
+    console.log("2. put on clothes suitable for athletic activity")
+}
+
+const getGear = () => {
+    console.log("3. put water bottle, cleats, frisbees, knee brace, and anything else in the car")
+}
+
+const drive = () => {
+    console.log("4. drive to the specified field from the GroupMe")
+}
+
+const cleatUp = () => {
+    console.log("5. unload gear from the car and put on cleats")
+}
+
+const stretch = () => {
+    console.log("6. stretch thoroughly because you're not 19 anymore")
+}
+
+const warmUp= () => {
+    console.log("7. warm up by practicing throws, then split up teams and play warm-up points")
+}
+
+const play = () => {
+    console.log("8. PLAY ULTIMATE!")
+}
+
+const stretchAgain = () => {
+    console.log("9. stretch again because you're still not 19")
+}
+
+const getBeers = () => {
+    console.log("10. go get beers at a brewery Rob picked")
+}
+
+const goHome = () => {
+    console.log("11. drive home while thinking about all the cool plays you made to tell your wife about")
+}
+
+const soreness = () => {
+    console.log("12. be sore for at least the next 2 days because once again, you're not 19")
+}
+
+const playUltimate = () => {
+    checkLocation()
+    getDressed()
+    getGear()
+    drive()
+    cleatUp()
+    stretch()
+    warmUp()
+    play()
+    stretchAgain()
+    getBeers()
+    goHome()
+    soreness()
+}
+
+playUltimate()
